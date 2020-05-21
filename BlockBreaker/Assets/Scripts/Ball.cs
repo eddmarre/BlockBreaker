275cd6d4ec4ffbea04b718d7129041c9200f72b4
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-21 12:56:34 -0400

    Start Game with M1
    +game starts once player presses mouse 1

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -6,8 +6,11 @@ public class Ball : MonoBehaviour
 {
     //configuration params
     [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 10f;
     //state 
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     private void Start()
     {
@@ -16,8 +19,26 @@ public class Ball : MonoBehaviour
 
     private void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
     }
 
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
 }
