397e50c0af02f06cbdac2b8783830a774627084f
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-21 12:27:38 -0400

    Moving Panel WIth Mouse Position
    +Clamped so Paddle Position is limited
    +Paddle follows mouse on screen

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [Header("configuration Parameters")]
+    [SerializeField] float screnWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    void Update()
+    {
+        //see the mouse position relative to the screen width.
+        //Debug.Log(Input.mousePosition.x / Screen.width * screnWidthInUnits);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screnWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
