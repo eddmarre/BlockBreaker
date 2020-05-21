75294eabddeb742c59c07100a53a74fb05b13cd1
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-21 12:38:10 -0400

    Stick Ball to Paddle for Start

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    
+    //configuration params
+    [SerializeField] Paddle paddle;
+    //state 
+    Vector2 paddleToBallVector;
+
+    private void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
 }
