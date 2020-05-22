18934f506b4fd440cf3d6442945cb36ee5f1d7a9
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-22 14:05:50 -0400

    Game Loop Finished
    +Added developer keys F to skip level
    +Game has a start and finish

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    [SerializeField] AudioClip breakSound;
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void Update() {
+        if(Debug.isDebugBuild)
+        {
+            DebugBinds();
+        }
+    }
+
+    private void DebugBinds()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            level.BlockDestroyed();
+            Destroy(gameObject);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.name == "ball")
         {
             //Debug.Log("hit by " + other.gameObject.name);
-            Destroy(gameObject);
+            DestroyBlock();
         }
 
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        level.BlockDestroyed();
+        Destroy(gameObject);
     }
 }
---
+++ BlockBreaker/Assets/Scripts/Level.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    int numberOfBlocks;
+
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+    public void CountBreakableBlocks()
+    {
+        numberOfBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        numberOfBlocks--;
+        if (numberOfBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
